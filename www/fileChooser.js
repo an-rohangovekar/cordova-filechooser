7710e415a1ebf140b01bffb40031db6bf58be32c
soyabshaikh <soyab.s@applicationnexus.com>
2015-02-13 16:40:44 +0530

    Update fileChooser.js

--- www/fileChooser.js
+++ www/fileChooser.js
@@ -1,14 +1,16 @@
-cordova.define("com.megster.cordova.FileChooser.FileChooser", function(require, exports, module) { module.exports = {
-    open: function (success, failure) {
+cordova.define("com.megster.cordova.FileChooser.FileChooser", function(require, exports, module) 
+{ 
+    
+    fileChooser.prototype.open= function (success, failure) {
         cordova.exec(success, failure, "FileChooser", "open", []);
-    },
-    play: function (success, failure,uri) {
+    };
+    fileChooser.prototype.play=function (success, failure,uri) {
    
         cordova.exec(success, failure, "FileChooser", "startPlaying", [uri]);
-    },
-    stop: function (success, failure) {
+    };
+    fileChooser.prototype.stop=function (success, failure) {
         cordova.exec(success, failure, "FileChooser", "stopPlaying", []);
-    }
-};
+    };
+module.exports = fileChooser;
 
 });
